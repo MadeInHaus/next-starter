cd8892bc35e055f710af789b30df7b85aa393c05
Claus Wahlers <claus@codeazur.com.br>
2023-02-02 04:49:51 -0300

    Add carousel demo

---
+++ pages/demos/carousel.js
@@ -0,0 +1,8 @@
+export async function getStaticProps() {
+    const api = 'https://dog.ceo/api/breed/schnauzer/miniature/images';
+    const apiResponse = await fetch(api);
+    const { message: dogs } = await apiResponse.json();
+    return { props: { dogs } };
+}
+
+export { default } from 'components/pages/demos/Carousel';
