ae944f5849dc09c4db74cfc3360ba1e5ce5e5442
Steve Scavo <stevescavo@gmail.com>
2023-02-27 09:42:05 -0800

    Change formatting

--- scripts/nvm.mjs
+++ scripts/nvm.mjs
@@ -4,15 +4,19 @@ import sh from 'shelljs';
 sh.config.silent = true;
 
 const run = async () => {
-    console.log(chalk.yellow.bold('Checking if NVM is installed... \n'));
+    console.log(
+        chalk.yellow.bold(
+            'Checking if Node Version Manager (nvm) is installed... \n'
+        )
+    );
 
     // Check for existence of NVM
     if (sh.exec('nvm')) {
-        console.log(chalk.green.bold('NVM is installed!'));
+        console.log(chalk.green.bold('nvm is installed!'));
         // Run nvm use
         sh.exec('nvm use');
     } else {
-        console.log(chalk.red.bold('NVM is not installed!'));
+        console.log(chalk.red.bold('nvm is not installed!'));
     }
 };
 
