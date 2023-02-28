1e53fbd98f9bbe500672cd4d31c9680180adf13e
Steve Scavo <stevescavo@gmail.com>
2023-02-27 17:08:35 -0800

    Check for required node version in package.json

---
+++ scripts/check-node-version.mjs
@@ -0,0 +1,34 @@
+import chalk from 'chalk';
+import pkg from '../package.json' assert { type: 'json' };
+
+const requiredNodeVersion = pkg.engines?.node;
+const currentVersion = process.versions.node;
+
+const requiredNodeVersionNumStr = requiredNodeVersion.replace(/[^\d.]/g, '');
+
+const requiredNodeVersionWholeNumber = Math.floor(
+    parseFloat(requiredNodeVersionNumStr)
+);
+const currentVersionWholeNumber = Math.floor(parseFloat(currentVersion));
+
+if (!requiredNodeVersion) {
+    // Create a space before and after this console log to make it stand out
+    console.warn(
+        chalk.yellow.bold(
+            '\nNo Node version specified in package.json. For more info, please visit https://docs.npmjs.com/cli/v8/configuring-npm/package-json#engines\n'
+        )
+    );
+} else if (requiredNodeVersionWholeNumber !== currentVersionWholeNumber) {
+    console.error(
+        chalk.red.bold(
+            `\nRequired Node version is ${requiredNodeVersion}, but you are using version ${currentVersion}. Please use Node Version Manager (nvm) to switch to a compatible version.\n`
+        )
+    );
+
+    // Exit the process with a non-zero code to indicate failure
+    process.exit(1);
+} else {
+    console.log(
+        chalk.green.bold(`\nNode version ${currentVersion} is compatible 👍`)
+    );
+}
