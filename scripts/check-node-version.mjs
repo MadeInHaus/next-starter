8ef3b81c197ca2d2aff63c702595e70e0f5e6387
Claus Wahlers <claus@codeazur.com.br>
2023-02-28 04:21:14 -0300

    Use node-semver to compare version numbers
    Also loads `package.json` the traditional way, because `import asserts` is only supported in Node >=16.14. Michelle would have run into an exception.
    
    I think the use of ESM should be okay, support is >=13.

--- scripts/check-node-version.mjs
+++ scripts/check-node-version.mjs
@@ -1,33 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
 import chalk from 'chalk';
-import pkg from '../package.json' assert { type: 'json' };
+import semver from 'semver';
 
-const requiredNodeVersion = pkg.engines?.node;
-const currentVersion = process.versions.node;
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+const fileName = path.resolve(__dirname, '../package.json');
+const pkg = loadPackageJson(fileName);
+const requiredNodeVersion = pkg?.engines?.node;
 
-const requiredNodeVersionNumStr = requiredNodeVersion.replace(/[^\d.]/g, '');
-
-const requiredNodeVersionWholeNumber = Math.floor(
-    parseFloat(requiredNodeVersionNumStr)
-);
-const currentVersionWholeNumber = Math.floor(parseFloat(currentVersion));
-
-if (!requiredNodeVersion) {
+if (!pkg) {
+    console.warn(
+        chalk.yellow.bold(`\nUnable to load package.json at ${fileName}\n`)
+    );
+} else if (!requiredNodeVersion) {
     console.warn(
         chalk.yellow.bold(
             '\nNo Node version specified in package.json. For more info, please visit https://docs.npmjs.com/cli/v8/configuring-npm/package-json#engines\n'
         )
     );
-} else if (requiredNodeVersionWholeNumber !== currentVersionWholeNumber) {
+} else if (!semver.satisfies(process.version, requiredNodeVersion)) {
     console.error(
         chalk.red.bold(
-            `\nRequired Node version is ${requiredNodeVersion}, but you are using version ${currentVersion}. Please use Node Version Manager (nvm) to switch to a compatible version.\n`
+            `\nRequired Node version is ${requiredNodeVersion}, but you are using version ${process.version}.\nPlease use Node Version Manager (nvm) to switch to a compatible version.\n`
         )
     );
-
     // Exit the process with a non-zero code to indicate failure
     process.exit(1);
 } else {
     console.log(
-        chalk.green.bold(`\nNode version ${currentVersion} is compatible 👍`)
+        chalk.green.bold(`\nNode version ${process.version} is compatible 👍\n`)
     );
+}
+
+function loadPackageJson(fileName) {
+    try {
+        const stat = fs.statSync(fileName, { throwIfNoEntry: false });
+        if (stat) {
+            return JSON.parse(fs.readFileSync(fileName, 'utf8'));
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    return null;
 }
